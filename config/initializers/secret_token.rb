62536ccd418ffc8470dbf2314b22bc5785f30647
tinasoul <tinapshah@gmail.com>
2013-08-04 17:00:38 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HairDid::Application.config.secret_token = 'c104404c96010567bb2efc53b9f9e769b1d5c233b1f9841d3dd07fb6858a74cf37fb4aebbfdf0c4b9a9a618a0030034b3ed5473b525715ca041ff9ca3618918b'
